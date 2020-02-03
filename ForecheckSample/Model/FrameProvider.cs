edc74c2067eec4ecb19638ccf0ead6e85030d94f
Dmitry Batukhtin <norfo@mail.ru>
2020-02-03 17:53:02 +0300

    Add functionality

--- ForecheckSample/Model/FrameProvider.cs
+++ ForecheckSample/Model/FrameProvider.cs
@@ -1,26 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using OpenCvSharp;
 
 namespace ForecheckSample.Model
 {
-    public class FrameProvider
+    public class FrameProvider : INotifyPropertyChanged
     {
         private string sourceVideoPath = "";
         private VideoCapture videoCapture;
+
+        public bool IsOpened { get; private set; } = false;
+        public int MaxFrameCount { get; private set; } = 0;
+        public int CurrentFrameCount { get; private set; } = 0;
+        public double Fps { get; private set; } = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public void DefineVideoSource(string sourcePath)
         {
             sourceVideoPath = sourcePath;
             videoCapture = new VideoCapture(sourcePath);
 
-            if (!videoCapture.IsOpened())
+            IsOpened = videoCapture.IsOpened();
+            if (IsOpened)
             {
-
+                MaxFrameCount = videoCapture.FrameCount;
+                CurrentFrameCount = 0;
+                Fps = videoCapture.Fps;
             }
+        }
+
+        public BitmapSource GetInitialFrame()
+        {
+            Mat frame = new Mat();
+            videoCapture.Read(frame);
+            return ConvertMatToBitmapSource(frame);
+        }
+
+        public BitmapSource GetNextFrame()
+        {
+            Mat frame = new Mat();
+            if (videoCapture.PosFrames < videoCapture.FrameCount)
+            {
+                CurrentFrameCount++;
+                videoCapture.Read(frame);
+                return ConvertMatToBitmapSource(frame);
+            }
+
+            return null;
+        }
+
+        private BitmapSource ConvertMatToBitmapSource(Mat image)
+        {
+            return OpenCvSharp.Extensions.BitmapSourceConverter.ToBitmapSource(image);
+        }
+
+        public void SetNewVideoPosition(int newPosition)
+        {
+            CurrentFrameCount = newPosition;
+            videoCapture.Set(CaptureProperty.PosFrames, newPosition);
         }
     }
 }
--- ForecheckSample/ViewModel/MainWindowVM.cs
+++ ForecheckSample/ViewModel/MainWindowVM.cs
@@ -1,4 +1,5 @@
-﻿using ForecheckSample.Services;
+﻿using ForecheckSample.Model;
+using ForecheckSample.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,25 +7,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace ForecheckSample.ViewModel
 {
     public class MainWindowVM : INotifyPropertyChanged
     {
+        private FrameProvider frameProvider = new FrameProvider();
+        private bool needToPlayVideo = false;
+        private DispatcherTimer timer;
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public BitmapSource Video { get; private set; }
+        public bool IsVideoOpen { get; set; } = false;
+        public int MaxFrameCount { get; set; } = 0;
+        public int CurrentFrameCount
+        {
+            get => frameProvider.CurrentFrameCount;
+            set
+            {
+                if (value >= 0 && value < MaxFrameCount)
+                {
+                    CurrentFrameCount = value;
+                    SetNewFramePosition(value);
+                }
+            }
+        }
 
         public ICommand OpenVideoFileCommand
         {
             get { return new RelayCommand((object obj) => OpenVideoFile()); }
         }
 
+        public ICommand PlayVideoCommand
+        {
+            get { return new RelayCommand((object obj) => PlayVideo()); }
+        }
+
         private void OpenVideoFile()
         {
-            var mDialogService = new DefaultDialogService();
-            if (mDialogService.OpenFileDialog())
+            var DialogService = new DefaultDialogService();
+            if (DialogService.OpenFileDialog())
             {
-
+                frameProvider.DefineVideoSource(DialogService.FilePath);
+                if (frameProvider.IsOpened)
+                {
+                    IsVideoOpen = true;
+                    MaxFrameCount = frameProvider.MaxFrameCount;
+                    CurrentFrameCount = frameProvider.CurrentFrameCount;
+                    Video = frameProvider.GetInitialFrame();
+                }
             }
+        }
+
+        private void PlayVideo()
+        {
+            if (!IsVideoOpen)
+                return;
+            needToPlayVideo = true;
+
+            timer = new DispatcherTimer();
+            timer.Tick += TimerTick; ;
+            timer.Interval = TimeSpan.FromMilliseconds(30);
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            var frame = frameProvider.GetNextFrame();
+            if (frame == null)
+                StopTimer();
+
+            Video = frame;
+            CurrentFrameCount = frameProvider.CurrentFrameCount;
+            if (CurrentFrameCount + 1 >= MaxFrameCount)
+                StopTimer();
+        }
+
+        private void SetNewFramePosition(int position)
+        {
+            frameProvider.SetNewVideoPosition(position);
+        }
+
+        private void StopTimer()
+        {
+            timer.Stop();
+            needToPlayVideo = false;
         }
     }
 }
